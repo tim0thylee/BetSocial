552b8331e5678e5bedb967607ef5babcbe953043
Joshua <joshuask23@gmail.com>
2018-04-07 01:57:57 -0700

    removed forgot password link

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -102,10 +102,6 @@ class Login extends Component {
                 Don't have an account?&nbsp;&nbsp;
             <Link to={"/register"}>Sign Up</Link>
               </p>
-              <p className="mt-5 mb-3">
-                Forgot Password?&nbsp;&nbsp;
-            <Link to={"/reset"}>Reset Password</Link>
-              </p>
             </form>
           </Col>
         </Row>
