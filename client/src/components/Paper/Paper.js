d7f25221adf9572743c6aa34e0dcfb52bcc3ee55
Joshua <joshuask23@gmail.com>
2018-04-07 05:27:47 -0700

    fixed paper

--- client/src/components/Paper/Paper.js
+++ client/src/components/Paper/Paper.js
@@ -6,7 +6,7 @@ import Paper from 'material-ui/Paper';
 const styles = theme => ({
   root: theme.mixins.gutters({
     paddingTop: 16,
-    paddingBottom: 16,
+    paddingBottom: 50,
     marginTop: theme.spacing.unit * 3,
   }),
 });
